d05d6bc7c6c3c219dbbf5ebd354692e784d57331
Patrícia Tayná <69331672+patriciatayna@users.noreply.github.com>
2026-05-28 23:33:11 -0300

    Update edit_tabela.sql

--- edit_tabela.sql
+++ edit_tabela.sql
@@ -1,96 +1,96 @@
--- CREATE DATABASE gerenciador_de_estudos;
--- acessar base de dados
-
 CREATE TABLE Departamento (
-	  cod_departamento CHAR(3) PRIMARY KEY,
-	  nome VARCHAR(20)
+  cod_departamento CHAR(3) PRIMARY KEY,
+  nome VARCHAR(20) UNIQUE NOT NULL
 );
 
 CREATE TABLE Professor (
-	  id_prof CHAR(8) primary key,
-    nome VARCHAR(30),
+  id_prof CHAR(8) primary key,
+  nome VARCHAR(30) NOT NULL,
     
-	  email VARCHAR(30) UNIQUE,
-    departamento CHAR(3),
-    sala VARCHAR(10),
+  email VARCHAR(30) UNIQUE,
+  departamento CHAR(3) NOT NULL,
+  sala VARCHAR(10),
 
-	  foreign key (departamento) references Departamento(cod_departamento)
+  foreign key (departamento) 
+  	references Departamento(cod_departamento)
 );
 
 CREATE TABLE Disciplina (
-  	cod_disciplina CHAR(7) primary key, 
-  	
-    nome VARCHAR(30), 
-  	quantidade_horas INT,
-  	departamento CHAR(3),
+  cod_disciplina CHAR(7) primary key, 
+ 
+  nome VARCHAR(30) NOT NULL, 
+  quantidade_horas INTEGER,
+  departamento CHAR(3),
   
-  	foreign key (departamento) references Departamento(cod_departamento)
+  foreign key (departamento) references Departamento(cod_departamento)
 );
 
 CREATE TABLE Turma (
-	  id_turma CHAR(8) primary key,
+  id_turma INTEGER PRIMARY KEY AUTOINCREMENT,
   
-  	professor CHAR(8),
-  	disciplina CHAR(7),
+  professor CHAR(8) NOT NULL,
+  disciplina CHAR(7) NOT NULL,
     horario VARCHAR(10),
     sala VARCHAR(10),
     
-	  foreign key (professor) references Professor(id_prof),
-	  foreign key (disciplina) references Disciplina(cod_disciplina)
+  foreign key (professor) references Professor(id_prof),
+  foreign key (disciplina) references Disciplina(cod_disciplina)
 );
-
--- -- --
--- 
--- -- --
-
--- CREATE TABLE Aluno (id_aluno, nome, matr, senha),
 
 CREATE TABLE Aluno 
 (
-  	id_aluno CHAR(9) PRIMARY KEY,
-  	nome VARCHAR(32),
-  	matr CHAR(9),
-  	senha VARCHAR(12)
+  id_aluno INTEGER PRIMARY KEY AUTOINCREMENT,
+
+  nome VARCHAR(32) NOT NULL,
+  matr CHAR(9) UNIQUE NOT NULL,
+
+  senha VARCHAR(12)
 );
 
--- CREATE TABLE Curso (id_curso, nome, turno, departamento),
 CREATE TABLE Curso 
 (
-  	id_curso CHAR(9) PRIMARY KEY,
-  	nome VARCHAR(32),
-  	turno CHAR(3),
-  	departamento VARCHAR(16)
+  id_curso INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome VARCHAR(32) NOT NULL,
+  turno CHAR(3),
+  departamento CHAR(3)
+
+     FOREIGN KEY (departamento)
+REFERENCES (Departamento.cod_departamento)
+
+     
 );
 
--- CREATE TABLE Avaliacao (id_avaliacao, data, peso, titulo, tipo),
 CREATE TABLE Avaliacao (
-  	id_avaliacao, 
-  	data, 
-  	peso, 
-  	titulo,
-  	tipo
+  id_avaliacao INTEGER PRIMARY KEY AUTOINCREMENT,
+  
+  data DATE NOT NULL, 
+  titulo VARCHAR(64) NOT NULL,
+  
+  tipo VARCHAR(24),
+  peso INTEGER
 );
--- CREATE TABLE Conteudo (id_conteudo, titulo, descricao),
+
 CREATE TABLE Conteudo 
 (
-  id_conteudo, 
-  titulo, 
-  descricao
+  id_conteudo INTEGER PRIMARY KEY AUTOINCREMENT, 
+  titulo VARCHAR(64) NOT NULL, 
+  descricao VARCHAR(204)
 );
 
--- CREATE TABLE PlanoEstudos (id_plano_estudos, tempo, status, data),
 CREATE TABLE PlanoEstudos 
 (
-  id_plano_estudos, 
-  tempo, 
-  status, 
-  data
+  id_plano_estudos INTEGER PRIMARY KEY AUTOINCREMENT, 
+  tempo INTEGER, 
+  status VARCHAR(12), 
+  data DATE
 );
--- CREATE TABLE FeedbackDiscip (id_fb, dificuldade, comentario, nota_prof);
+
 CREATE TABLE FeedbackDiscip 
 (
-  id_fb, 
-  dificuldade, 
-  comentario, 
-  nota_prof
+  id_fb INTEGER PRIMARY KEY AUTOINCREMENT,
+  dificuldade VARCHAR(12), 
+  comentario VARCHAR(204), 
+  nota_prof INTEGER
+  
+  -- 1 a 10 ou 1 a 5? (restrição)
 );
